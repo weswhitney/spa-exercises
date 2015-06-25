3ed7284e8f8393f3f27656280cf57ab2ec9fb025
weswhitney <wesleywhitney@gmail.com>
2015-06-25 09:19:59 -0600

    Added some jquery exercises

---
+++ jquery.js
@@ -0,0 +1,11 @@
+/*jslint browser : true, continue : true,
+  devel  : true, indent : 2,    maxerr   : 50,
+  newcap : true, nomen  : true, plusplus : true,
+  regexp : true, sloppy : true, vars : true,
+  white  : true
+*/
+/*global jQuery */
+
+jQuery(document).ready(function () {
+  jQuery("h1").text("who you with?");
+});
